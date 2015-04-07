6109e1acc0b91eccba3bf8727c031e4d79ae147b
sanath <sanath@ufl.edu>
2015-04-07 17:03:45 -0400

    Addign Pagination for Events table

--- app/redidropper/static/js/technician.js
+++ app/redidropper/static/js/technician.js
@@ -34,9 +34,25 @@ function getSubjectsList(){
                         ]};
 }
 
+function getNewSubjectsList(){
+       return {
+        max_events:12,
+        subjects_data:
+            [
+                {subject_id:"7",subject_name:"Subject 7",events:[{event_id:23,event_files:100}]},
+                {subject_id:"8",subject_name:"Subject 8",events:[{event_id:23,event_files:50},{event_id:23,event_files:30},{event_id:23,event_files:30},{event_id:23,event_files:30},{event_id:23,event_files:30},{event_id:23,event_files:30}]},
+                {subject_id:"9",subject_name:"Subject 9",events:[{event_id:23,event_files:30},{event_id:23,event_files:30},{event_id:23,event_files:30},{event_id:23,event_files:30}]},
+                {subject_id:"10",subject_name:"Subject 10",events:[{event_id:23,event_files:10},{event_id:23,event_files:30},{event_id:23,event_files:30}]},
+                {subject_id:"11",subject_name:"Subject 11",events:[{event_id:23,event_files:16}]},
+                {subject_id:"12",subject_name:"Subject 12",events:[{event_id:23,event_files:18}]}
+            ]};
+}
 var SubjectsRow = React.createClass({
   getInitialState: function() {
     return {row_data:this.props.row_data,max_events:this.props.max_events};
+  },
+  componentWillReceiveProps:function(nextProps){
+    this.setState({row_data:nextProps.row_data,max_events:nextProps.max_events});
   },
   showAlert:function(){
     $("#event-alert").show();
@@ -50,7 +66,6 @@ var SubjectsRow = React.createClass({
     var row_data=this.state.row_data;
     var events_count=row_data.events.length;
     for(var i=0;i<events_count;i++){
-        console.log(this.props.row_data.subject_id);
         var view_files_url="/users/manage_event/"+row_data.events[i].event_id;
         if (row_data.events[i].event_files!=0){
             table_columns.push(<td><a href={view_files_url}>{row_data.events[i].event_files}</a></td>);
@@ -63,7 +78,6 @@ var SubjectsRow = React.createClass({
     for(var i=events_count+2;i<=column_count;i++){
         table_columns.push(<td><i className="fa fa-lg fa-plus-circle" onClick={this.showAlert}></i></td>);
     }
-
     return (
         <tr>
         <td>{row_data.subject_id}</td>
@@ -74,9 +88,19 @@ var SubjectsRow = React.createClass({
 }
 });
 
+
 var SubjectsTable = React.createClass({
   getInitialState: function() {
     return {subjects:getSubjectsList()};
+  },
+  changePage:function(i){
+    var data ;
+    if(i%2==0){
+        data= getSubjectsList();
+    }else{
+        data= getNewSubjectsList();
+    }
+    this.setState({subjects:data});
   },
   componentWillMount:function(){
     /*
@@ -106,6 +130,14 @@ var SubjectsTable = React.createClass({
         table_rows.push(<SubjectsRow row_data={subjects_data[i]} max_events={column_count}/>);
     }
 
+    var pagination ;
+    var no_of_pages = 10
+    if(this.state.subjects.length<3){
+
+    }else{
+        pagination=<SubjectsPagination no_of_pages={no_of_pages} changePage={this.changePage}/>;
+    
+    }
     return (
     <div className="table-responsive">
         <div>{this.props.selected_project}</div>
@@ -119,10 +151,71 @@ var SubjectsTable = React.createClass({
                 {table_rows}
             </tbody>
         </table>
+        {pagination}
     </div>
     );
   }
 });
+
+var SubjectsPagination =React.createClass({
+  getInitialState: function() {
+    return {no_of_pages:this.props.no_of_pages,current_page:1};
+  },
+  componentWillReceiveProps:function(nextProps){
+       // this.setState({list_of_files:nextProps.list_of_files,visibility:nextProps.visibility});
+  },
+  activateOnClick:function(i){
+    this.setState({no_of_pages:this.state.no_of_pages,current_page:i});
+    this.props.changePage(i);
+  },
+  nextPage:function(){
+    var current_page=this.state.current_page;
+    if(current_page==this.state.no_of_pages){
+        return;
+    }else{
+        this.setState({no_of_pages:this.state.no_of_pages,current_page:current_page+1});
+        this.props.changePage(current_page+1);
+    }
+  },
+  prevPage:function(){
+    var current_page=this.state.current_page;
+    if(current_page==1){
+        return;
+    }else{
+        this.setState({no_of_pages:this.state.no_of_pages,current_page:current_page-1});
+        this.props.changePage(current_page-1);
+    }
+  },
+  render: function() {
+    var pages=[];
+  
+    for(var i=1;i<=this.state.no_of_pages;i++){
+        if(i==this.state.current_page){
+            pages.push(<li className="active"><a>{i}</a></li>);
+        }else{
+            pages.push(<li><a onClick={this.activateOnClick.bind(null,i)}>{i}</a></li>);
+        }
+    }
+    return (
+    <nav>
+      <ul className="pagination">
+        <li>
+          <a onClick={this.prevPage} aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        {pages}
+        <li>
+          <a onClick={this.nextPage} aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>  
+    );
+  }
+});
+
 
 var Technician = React.createClass({
   getInitialState: function() {
@@ -143,6 +236,9 @@ var Technician = React.createClass({
     console.log("select changed "+this.refs.project_select.getDOMNode().value);
     var new_selected_value = this.refs.project_select.getDOMNode().value;
     this.setState({projects:this.state.projects,selected_project:new_selected_value});
+  },
+  changePage:function(){
+
   },
   render: function() {
 
