ca919ab77f83fb8536170538e5c18b76823da2fc
Andrei Sura <sura.andrei@gmail.com>
2015-04-20 12:35:33 -0400

    Update schema + remove Project table + add data for subject files + add data vor log events

--- app/db/002/data.sql
+++ app/db/002/data.sql
@@ -1,137 +1,121 @@
 
 USE RediDropper;
 
+SET @end = CONCAT(CURDATE() + interval 6 month, ' 23:59:59');
+
+INSERT INTO User (usrEmail, usrFirst, usrLast, usrAddedAt, usrAccessExpiresAt)
+VALUES
+    ('admin@example.com', 'Admin', 'Adminsky', NOW(), @end),
+    ('technician@example.com', 'Technician', 'Țărnă', NOW(), @end),
+    ('researcher_one@example.com', 'Researcher 1', 'de Méziriac', NOW(), @end),
+    ('researcher_two@example.com', 'Researcher 2', 'Bauchspeicheldrüsenkrebs',NOW(), @end)
+;
+
+
 
 INSERT INTO Role (rolName, rolDescription)
 VALUES
-    ('admin', 'Can edit users, roles, projects'),
+    ('admin', 'Can add/edit users, roles, log events; upload/delete images'),
     ('technician', 'Can upload/delete images'),
     ('researcher_one', 'Can upload/download images'),
     ('researcher_two', 'Can upload/download images')
 ;
 
 
-INSERT INTO User (usrEmail, usrFirst, usrLast, usrAddedAt)
-VALUES
-    ('admin@example.com', 'Admin', 'Adminsky', NOW())
-;
 
-INSERT INTO User (usrEmail, usrFirst, usrLast, usrAddedAt)
-VALUES
-    ('technician@example.com', 'Technician', 'Țărnă', NOW())
-;
-
-INSERT INTO User (usrEmail, usrFirst, usrLast, usrAddedAt)
-VALUES
-    ('researcher_one@example.com', 'Researcher 1', 'de Méziriac', NOW())
-;
-
-INSERT INTO User (usrEmail, usrFirst, usrLast, usrAddedAt)
-VALUES
-    ('researcher_two@example.com', 'Researcher 2', 'Bauchspeicheldrüsenkrebs',NOW())
-;
-
-
-INSERT INTO Project (prjUrlHost, prjUrlPath, prjName, prjApiKey, prjAddedAt)
-VALUES
-    ('http://localhost:8081', '/redcap/redcap_v6.0.5/ProjectSetup/index.php?pid=34', 'ADRC TEST', 'FC25694A0BFD3602362992E12DC89DB3', NOW()),
-    ('http://localhost:8998', '/redcap_v5.7.4/ProjectSetup/index.php?pid=12', 'PMP TEST', '121212', NOW())
-;
-
-
-INSERT INTO ProjectUserRole (prjID, usrID, rolID)
-SELECT
-    prjID, usrID, rolID
-FROM
-    Project, User, Role
-WHERE
-    prjName = 'ADRC TEST'
-    AND usrEmail = 'admin@example.com'
-    AND rolName = 'admin'
-;
-
-INSERT INTO ProjectUserRole (prjID, usrID, rolID)
-SELECT
-    prjID, usrID, rolID
-FROM
-    Project, User, Role
-WHERE
-    prjName = 'ADRC TEST'
-    AND usrEmail = 'technician@example.com'
-    AND rolName = 'technician'
-;
-
-
-INSERT INTO ProjectUserRole (prjID, usrID, rolID)
-SELECT
-    prjID, usrID, rolID
-FROM
-    Project, User, Role
-WHERE
-    prjName = 'ADRC TEST'
-    AND usrEmail = 'researcher_one@example.com'
-    AND rolName = 'researcher_one'
-;
-
-INSERT INTO ProjectUserRole (prjID, usrID, rolID)
-SELECT
-    prjID, usrID, rolID
-FROM
-    Project, User, Role
-WHERE
-    prjName = 'ADRC TEST'
-    AND usrEmail = 'researcher_two@example.com'
-    AND rolName = 'researcher_two'
-;
-
-INSERT INTO UserAuth (usrID, uathUsername, uathSalt, uathPassword)
-SELECT
-    usrID, 'admin', 'dc2d541d7eedf358', 'T0hgkGvsw8P5UPYL22bn9Ez4MDdqqDBZ48QfydAankYHkwMjeim5j4G64R/QcrVO4BZ96veIDDHWriVfmRmS+Q=='
-FROM
-    User
-WHERE
-    usrEmail = 'admin@example.com'
-;
-
-INSERT INTO UserAuth (usrID, uathUsername, uathSalt, uathPassword)
-SELECT
-    usrID, 'technician', '71b67ec3450e2997', '/ezLEEurS8q8bQju/Sk5BfU0dK+2/nptUSTlICsPBO8C/wW+VF6WzGGOOVyVI/Bk6tRjlOZunjMB13M3vUxrlg=='
-FROM
-    User
-WHERE
-    usrEmail = 'technician@example.com'
-;
-
--- second project
-INSERT INTO ProjectUserRole (prjID, usrID, rolID)
-SELECT
-    prjID, usrID, rolID
-FROM
-    Project, User, Role
-WHERE
-    prjName = 'PMP TEST'
-    AND usrEmail = 'admin@example.com'
-    AND rolName = 'technician'
+INSERT INTO UserRole (usrID, rolID, urAddedAt)
+      SELECT usrID, rolID, NOW() FROM User, Role WHERE usrEmail = 'admin@example.com' AND rolName = 'admin'
+UNION SELECT usrID, rolID, NOW() FROM User, Role WHERE usrEmail = 'technician@example.com' AND rolName = 'technician'
+UNION SELECT usrID, rolID, NOW() FROM User, Role WHERE usrEmail = 'researcher_one@example.com' AND rolName = 'researcher_one'
+UNION SELECT usrID, rolID, NOW() FROM User, Role WHERE usrEmail = 'researcher_two@example.com' AND rolName = 'researcher_two'
 ;
 
 -- Subjects
-INSERT INTO Subject (prjID, sbjRedcapID, sbjAddedAt)
-SELECT
-    prjID, '001', NOW() FROM Project WHERE prjID = 1
-;
-
-INSERT INTO Subject (prjID, sbjRedcapID, sbjAddedAt)
-SELECT
-    prjID, '002', NOW() FROM Project WHERE prjID = 1
+INSERT INTO Subject (sbjRedcapID, sbjAddedAt)
+VALUES
+    ('001', NOW()),
+    ('002', NOW()),
+    ('003', NOW()),
+    ('004', NOW()),
+    ('005', NOW())
 ;
 
 
 -- Subject Files
-INSERT INTO SubjectFile 
-    (sbjID, sfEventNumber, sfFileName, sfFileCheckSum, sfUploadDate, usrID)
-SELECT
-    1, 1, 'file.png', md5('test'), NOW(), 1
+INSERT INTO SubjectFile (sbjID, sfEventNumber, sfFileName, sfFileCheckSum, sfUploadDate, usrID)
+        SELECT 1, 1, 'file.png',  md5('a'), NOW(), 1
+UNION   SELECT 1, 1, 'file2.png', md5('b'), NOW(), 1
+UNION   SELECT 1, 1, 'file3.png', md5('c'), NOW(), 1
+UNION   SELECT 1, 2, 'file4.png', md5('d'), NOW(), 1
+UNION   SELECT 1, 2, 'file5.png', md5('e'), NOW(), 1
+UNION   SELECT 1, 2, 'file6.png', md5('f'), NOW(), 1
+UNION   SELECT 1, 3, 'file7.png', md5('g'), NOW(), 1
+UNION   SELECT 1, 3, 'file8.png', md5('h'), NOW(), 1
+UNION   SELECT 1, 4, 'file9.png', md5('i'), NOW(), 1
+UNION   SELECT 1, 5, 'fileA.png', md5('j'), NOW(), 1
+UNION   SELECT 1, 6, 'fileB.png', md5('k'), NOW(), 1
+UNION   SELECT 1, 7, 'fileC.png', md5('l'), NOW(), 1
+UNION   SELECT 1, 7, 'fileD.png', md5('m'), NOW(), 1
+UNION   SELECT 1, 8, 'fileE.png', md5('n'), NOW(), 1
+UNION   SELECT 1, 8, 'fileF.png', md5('o'), NOW(), 1
+UNION   SELECT 1, 9, 'fileG.png', md5('p'), NOW(), 1
+UNION   SELECT 1, 9, 'fileH.png', md5('q'), NOW(), 1
+
+UNION   SELECT 2, 1, 'file.png',  md5('a'), NOW(), 1
+UNION   SELECT 2, 2, 'file2.png', md5('b'), NOW(), 1
+UNION   SELECT 2, 2, 'file3.png', md5('c'), NOW(), 1
+UNION   SELECT 2, 2, 'file4.png', md5('d'), NOW(), 1
+UNION   SELECT 2, 2, 'file5.png', md5('e'), NOW(), 1
+UNION   SELECT 2, 2, 'file6.png', md5('f'), NOW(), 1
+UNION   SELECT 2, 3, 'file7.png', md5('g'), NOW(), 1
+UNION   SELECT 2, 3, 'file8.png', md5('h'), NOW(), 1
+UNION   SELECT 2, 4, 'file9.png', md5('i'), NOW(), 1
+UNION   SELECT 2, 5, 'fileA.png', md5('j'), NOW(), 2
+UNION   SELECT 2, 5, 'fileB.png', md5('k'), NOW(), 2
+UNION   SELECT 2, 5, 'fileC.png', md5('l'), NOW(), 2
+UNION   SELECT 2, 5, 'fileD.png', md5('m'), NOW(), 2
+UNION   SELECT 2, 5, 'fileE.png', md5('n'), NOW(), 2
+UNION   SELECT 2, 6, 'fileF.png', md5('o'), NOW(), 2
+UNION   SELECT 2, 7, 'fileG.png', md5('p'), NOW(), 2
+UNION   SELECT 2, 7, 'fileH.png', md5('q'), NOW(), 2
+
+UNION   SELECT 3, 1, 'file.png',  md5('a'), NOW(), 1
+UNION   SELECT 3, 2, 'file2.png', md5('b'), NOW(), 1
+UNION   SELECT 3, 2, 'file3.png', md5('c'), NOW(), 1
+UNION   SELECT 3, 2, 'file4.png', md5('d'), NOW(), 1
+UNION   SELECT 3, 2, 'file5.png', md5('e'), NOW(), 1
+UNION   SELECT 3, 2, 'file6.png', md5('f'), NOW(), 1
+UNION   SELECT 3, 3, 'file7.png', md5('g'), NOW(), 1
+UNION   SELECT 3, 3, 'file8.png', md5('h'), NOW(), 1
+UNION   SELECT 3, 3, 'file9.png', md5('i'), NOW(), 1
+UNION   SELECT 3, 3, 'fileA.png', md5('j'), NOW(), 2
+UNION   SELECT 3, 3, 'fileB.png', md5('k'), NOW(), 2
+UNION   SELECT 3, 3, 'fileC.png', md5('l'), NOW(), 2
+UNION   SELECT 3, 4, 'fileD.png', md5('m'), NOW(), 2
+UNION   SELECT 3, 4, 'fileE.png', md5('n'), NOW(), 2
+UNION   SELECT 3, 5, 'fileF.png', md5('o'), NOW(), 2
+UNION   SELECT 3, 6, 'fileG.png', md5('p'), NOW(), 2
+UNION   SELECT 3, 7, 'fileH.png', md5('q'), NOW(), 2
+
+UNION   SELECT 4, 1, 'file.png',  md5('a'), NOW(), 1
+UNION   SELECT 4, 1, 'file2.png', md5('b'), NOW(), 1
+UNION   SELECT 4, 2, 'file3.png', md5('c'), NOW(), 1
+UNION   SELECT 4, 3, 'file4.png', md5('d'), NOW(), 1
+UNION   SELECT 4, 3, 'file5.png', md5('e'), NOW(), 1
+UNION   SELECT 4, 3, 'file6.png', md5('f'), NOW(), 1
+UNION   SELECT 4, 4, 'file7.png', md5('g'), NOW(), 1
+UNION   SELECT 4, 5, 'file8.png', md5('h'), NOW(), 1
+UNION   SELECT 4, 6, 'file9.png', md5('i'), NOW(), 1
+UNION   SELECT 4, 7, 'fileA.png', md5('j'), NOW(), 2
+UNION   SELECT 4, 8, 'fileB.png', md5('k'), NOW(), 2
+UNION   SELECT 4, 3, 'fileC.png', md5('l'), NOW(), 2
+UNION   SELECT 4, 9, 'fileD.png', md5('m'), NOW(), 2
+UNION   SELECT 4,10, 'fileE.png', md5('n'), NOW(), 2
+UNION   SELECT 4,11, 'fileF.png', md5('o'), NOW(), 2
+UNION   SELECT 4,12, 'fileG.png', md5('p'), NOW(), 2
+UNION   SELECT 4,13, 'fileH.png', md5('q'), NOW(), 2
 ;
+
 
 -- Logging
 
@@ -145,4 +129,21 @@ VALUES
     ('login_error', ''),
     ('file_uploaded', ''),
     ('file_downloaded', '')
+;
+
+
+INSERT INTO WebSession (webSessID, usrID, webIP, webDateTime)
+VALUES
+    (md5('ha'), 1, '192.168.1.1', NOW()),
+    (md5('ha2'), 1, '172.27.1.100', NOW())
+;
+
+
+INSERT INTO Event (evttID, evtIP, webID, evtDateTime)
+      SELECT evttID, '1.2.3.4', 1, NOW() FROM EventType WHERE evttType = 'account_created'
+UNION SELECT evttID, '1.2.3.4', 1, NOW() FROM EventType WHERE evttType = 'login'
+UNION SELECT evttID, '1.2.3.4', 1, NOW() FROM EventType WHERE evttType = 'logout'
+UNION SELECT evttID, '1.2.3.4', 1, NOW() FROM EventType WHERE evttType = 'login_error'
+UNION SELECT evttID, '1.2.3.4', 1, NOW() FROM EventType WHERE evttType = 'file_uploaded'
+UNION SELECT evttID, '1.2.3.4', 1, NOW() FROM EventType WHERE evttType = 'file_downloaded'
 ;
--- app/db/002/upgrade.sql
+++ app/db/002/upgrade.sql
@@ -2,13 +2,16 @@
 USE RediDropper;
 
 INSERT INTO Version (verID, verInfo)
-    VALUES('002', 'Create tables: User, UserAuth, Role, Project, ProjectUserRole')
+    VALUES('002', 'Create tables: User, Role, UserRole, Subject, SubjectFile, UserAgent, WebSession, EventType, Event')
 ;
 
 
 -- http://docs.sqlalchemy.org/en/latest/orm/extensions/automap.html
 
 -- Store user's personal info
+-- usrPasswordResetToken varchar(255) NOT NULL DEFAULT '',
+-- usrEmailConfirmationToken varchar(255) NOT NULL DEFAULT '',
+
 CREATE TABLE User (
     usrID integer unsigned NOT NULL AUTO_INCREMENT,
     usrEmail varchar(255) NOT NULL DEFAULT '',
@@ -19,12 +22,15 @@ CREATE TABLE User (
     usrModifiedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     usrEmailConfirmedAt datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
     usrIsActive tinyint NOT NULL DEFAULT 1,
+    usrAccessExpiresAt datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+    usrPasswordHash varchar(255) NOT NULL DEFAULT '',
  PRIMARY KEY (usrID),
  UNIQUE KEY (usrEmail),
  KEY (usrFirst, usrLast),
  KEY (usrAddedAt),
  KEY (usrModifiedAt),
- KEY (usrEmailConfirmedAt)
+ KEY (usrEmailConfirmedAt),
+ KEY(usrAccessExpiresAt)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 ;
 
@@ -38,66 +44,30 @@ CREATE TABLE Role (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 ;
 
--- Store project name, id, token (assumes only one token per project)
-CREATE TABLE Project (
-    prjID smallint unsigned NOT NULL AUTO_INCREMENT,
-    prjName varchar(255) NOT NULL,
-    prjUrlHost varchar(255) NOT NULL,
-    prjUrlPath varchar(255) NOT NULL,
-    prjApiKey varchar(255) NOT NULL DEFAULT '',
-    prjAddedAt datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-    prjModifiedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
- PRIMARY KEY (prjID),
- UNIQUE KEY (prjUrlHost, prjUrlPath),
- KEY (prjName),
- KEY (prjAddedAt),
- KEY (prjModifiedAt)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
-;
-
 -- Store user roles mapping
-CREATE TABLE ProjectUserRole (
-    purID integer unsigned NOT NULL AUTO_INCREMENT,
-    prjID smallint unsigned NOT NULL,
+CREATE TABLE UserRole (
+    urID integer unsigned NOT NULL AUTO_INCREMENT,
     usrID integer unsigned NOT NULL,
     rolID smallint unsigned NOT NULL,
- PRIMARY KEY (purID),
- UNIQUE KEY(prjID, usrID),
- CONSTRAINT `fk_ProjectUserRole_prjID` FOREIGN KEY (prjID) REFERENCES Project (prjID) ON DELETE CASCADE,
- CONSTRAINT `fk_ProjectUserRole_usrID` FOREIGN KEY (usrID) REFERENCES User (usrID) ON DELETE CASCADE,
- CONSTRAINT `fk_ProjectUserRole_rolID` FOREIGN KEY (rolID) REFERENCES Role (rolID) ON DELETE CASCADE
+    urAddedAt datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ PRIMARY KEY (urID),
+ CONSTRAINT `fk_User_usrID` FOREIGN KEY (usrID) REFERENCES User (usrID) ON DELETE CASCADE,
+ CONSTRAINT `fk_UserRole_rolID` FOREIGN KEY (rolID) REFERENCES Role (rolID) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 ;
 
-CREATE TABLE UserAuth (
-    uathID integer unsigned NOT NULL AUTO_INCREMENT,
-    usrID integer unsigned NOT NULL,
-    uathUsername varchar(255) NOT NULL,
-    uathSalt varchar(255) NOT NULL,
-    uathPassword varchar(255) NOT NULL,
-    uathPasswordResetToken varchar(255) NOT NULL DEFAULT '',
-    uathEmailConfirmationToken varchar(255) NOT NULL DEFAULT '',
-    uathModifiedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
- PRIMARY KEY (uathID),
- UNIQUE KEY (usrID),
- UNIQUE KEY (uathUsername),
- CONSTRAINT `fk_UserAuth_usrID` FOREIGN KEY (usrID) REFERENCES User (usrID) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
-;
-
+-- show active users but does not filter out the ones with usrAccessExpiresAt < NOW()
 CREATE
     ALGORITHM=UNDEFINED
     DEFINER=`redidropper`@`localhost`
-    VIEW `ProjectUserRoleView`
+    VIEW `user_role_view`
 AS
 SELECT
-    prjID, prjName, usrID, usrEmail, rolID, rolName, uathID, uathUsername, uathPassword
+    usrID, usrEmail, rolID, rolName, urAddedAt, usrAccessExpiresAt
 FROM
     User
-    JOIN ProjectUserRole USING (usrID)
-    JOIN Project USING (prjID)
+    JOIN UserRole USING (usrID)
     JOIN Role USING (rolID)
-    LEFT JOIN UserAuth USING(usrID)
 WHERE
     usrIsActive
 ;
@@ -105,14 +75,11 @@ WHERE
 
 CREATE TABLE Subject (
     sbjID int(10) unsigned NOT NULL AUTO_INCREMENT,
-    prjID smallint unsigned NOT NULL,
     sbjRedcapID varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
     sbjAddedAt datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
  PRIMARY KEY (sbjID),
- UNIQUE KEY(prjID, sbjRedcapID),
- KEY (sbjRedcapID),
- KEY (sbjAddedAt),
- CONSTRAINT `fk_Subject_prjID` FOREIGN KEY (prjID) REFERENCES Project (prjID) ON DELETE CASCADE
+ UNIQUE KEY(sbjRedcapID),
+ KEY (sbjAddedAt)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 ;
 
@@ -132,6 +99,22 @@ CREATE TABLE SubjectFile (
  KEY (usrID),
  CONSTRAINT `fk_SubjectFile_usrID` FOREIGN KEY (usrID) REFERENCES User (usrID)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+;
+
+CREATE
+    ALGORITHM=UNDEFINED
+    DEFINER=`redidropper`@`localhost`
+    VIEW `subject_file_view`
+AS
+SELECT
+    sbjID, sbjRedcapID, sfEventNumber, COUNT(sfFileName) AS totalEventFiles
+FROM
+    Subject
+    JOIN SubjectFile USING (sbjID)
+GROUP BY
+    sbjRedcapID, sfEventNumber
+ORDER BY
+    sbjRedcapID
 ;
 
 
