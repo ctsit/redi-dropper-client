07aee4936d4fbb55293e3c5d5362137021e2425f
Andrei Sura <sura.andrei@gmail.com>
2015-04-24 13:09:43 -0400

    security: use a ssl context in debug mode

--- app/run.py
+++ app/run.py
@@ -10,16 +10,37 @@ Goal: Implement the application entry point
 from redidropper.main import app, db
 from redidropper.startup import initializer
 
-
 # Configures routes, models
 app = initializer.do_init(app, db)
 
+ssl_public_key_file = 'server.crt'
+ssl_private_key_file = 'server.key'
+
+
+def get_ssl_context(app):
+    """
+    Get a SSL context in debug mode
+    @see http://werkzeug.pocoo.org/docs/0.10/serving/#quickstart
+    """
+    import os.path
+    import ssl
+
+    if app.debug:
+        if os.path.isfile(ssl_public_key_file) and \
+                os.path.isfile(ssl_private_key_file):
+            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+            ssl_context.load_cert_chain(ssl_public_key_file,
+                                        ssl_private_key_file)
+        else:
+            try:
+                import OpenSSL
+                # if the pyOpenSSL is installed use the adhoc ssl context
+                ssl_context = 'adhoc'
+            except:
+                pass
+    return ssl_context
+
 
 if __name__ == "__main__":
-    app.run()
-
-    # from redidropper.database import db_manager
-    # run the server if executed from the command line
-    # with db_manager.session_scope() as session:
-    #    app.db_session = session
-    #    app.run()
+    ssl_context = get_ssl_context(app)
+    app.run(ssl_context=ssl_context)
