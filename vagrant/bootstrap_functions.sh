9712d523a011d76cf8e6d75163ecc170473d642c
Andrei Sura <sura.andrei@gmail.com>
2015-06-16 15:48:41 -0400

    Update vagrant config to match the staging updates

--- vagrant/bootstrap_functions.sh
+++ vagrant/bootstrap_functions.sh
@@ -21,19 +21,25 @@ function install_apache_for_python() {
 
    # Setting up a virtual environment will keep the application and its dependencies isolated from the main system.
    # Changes to it will not affect the cloud server's system configurations.
-   pip install virtualenv
+   pip install virtualenv fabric
 }
 
+# The vagrant uses the dedicated startup script ==> vagrant.wsgi
+# in order to avoid changing the default ==> dropper.wsgi
+#
+# Note: Apache is configured to set the python-path
+#   ==> /var/www/app/venv/lib/python2.7/site-packages
 function install_dropper() {
    pushd /var/www/app
 
-      sed -i'.bak' 's#/srv/apps/dropper-alz#/var/www#' ./deploy/dropper.wsgi
-
+      echo "Creating virtual environment: /var/www/app/venv"
       virtualenv venv
       source venv/bin/activate
-         pip install fabric
-         fab install_requirements
-         #fab init_db
+         echo "Installing application required packages..."
+         pip install -r requirements/dev.txt
+
+         echo "Creating database and tables..."
+         sleep 2
          mysql < db/001/upgrade.sql
          mysql < db/002/upgrade.sql
          mysql < db/002/data.sql
@@ -45,13 +51,20 @@ function install_dropper() {
       echo "DB_USER = 'root'" >> ./deploy/settings.conf
       echo "DB_PASS = ''" >> ./deploy/settings.conf
 
+      echo "Stop apache in order to disable the default site"
       service apache2 stop
       a2dissite 000-default
+
+      echo "Link config files for apache port 80 and 443"
       ln -s /vagrant/apache.conf /etc/apache2/sites-available/dropper.conf
       ln -s /vagrant/apache-ssl.conf /etc/apache2/sites-available/dropper-ssl.conf
       ln -s /vagrant/apache.conf /etc/apache2/sites-enabled/dropper.conf
       ln -s /vagrant/apache-ssl.conf /etc/apache2/sites-enabled/dropper-ssl.conf
       a2enmod ssl
+      a2enmod headers
+
+      echo "Restaring Apache with new config..."
+      sleep 2
       service apache2 start
 
    popd
@@ -63,12 +76,9 @@ function install_utils() {
    cp $SHARED_FOLDER/vimrc /home/vagrant/.vimrc
    cp $SHARED_FOLDER/vimrc /root/.vimrc
    apt-get install -y \
-      vim vim-runtime \
-      ctags vim-doc vim-scripts \
+      vim \
       ack-grep \
       git \
-      curl \
-      nmap \
-      tree
+      curl
 }
 
