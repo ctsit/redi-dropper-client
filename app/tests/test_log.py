e80777cce6c65ed8bcbdf1622b5620f9852bed3a
Andrei Sura <sura.andrei@gmail.com>
2015-06-25 22:07:36 -0400

    Fix unit tests

--- app/tests/test_log.py
+++ app/tests/test_log.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-# from redidropper.main import db
+from redidropper.models.log_type_entity import LOG_TYPE_LOGIN, LOG_TYPE_LOGOUT
 from redidropper.models.log_type_entity import LogTypeEntity
 from redidropper.models.log_entity import LogEntity
 from redidropper.models.web_session_entity import WebSessionEntity
@@ -11,18 +11,25 @@ from .base_test import BaseTestCase
 class LogTests(BaseTestCase):
 
     def test_log_creation(self):
-        log_type = LogTypeEntity.create(type='login', description='')
-        self.assertEquals(1, log_type.id)
-        # print(log_type)
+        log_login = LogTypeEntity.create(type=LOG_TYPE_LOGIN, description='')
+        log_logout = LogTypeEntity.create(type=LOG_TYPE_LOGOUT, description='')
+        self.assertEquals(1, log_login.id)
+        self.assertEquals(2, log_logout.id)
 
         web_session = WebSessionEntity.create()
         self.assertEquals(1, web_session.id)
 
-        log = LogEntity.create(type_id=log_type.id,
+        log = LogEntity.create(type_id=log_login.id,
                                web_session_id=web_session.id,
                                date_time=datetime.now(),
                                details='just a test')
+        log2 = LogEntity.create(type_id=log_logout.id,
+                                web_session_id=web_session.id,
+                                date_time=datetime.now(),
+                                details='just a test')
 
         self.assertEquals(1, log.id)
-        self.assertEquals("login", log.log_type.type)
+        self.assertEquals(2, log2.id)
+        self.assertEquals(LOG_TYPE_LOGIN, log.log_type.type)
+        self.assertEquals(LOG_TYPE_LOGOUT, log2.log_type.type)
         # print(log)
