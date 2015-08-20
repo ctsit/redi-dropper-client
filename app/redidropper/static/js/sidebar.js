de145171b0d83c5cae37a84247285a4eb2a35cb3
Ruchi Vivek Desai <ruchi.desai@ufl.edu>
2015-08-20 10:41:19 -0400

    Change the icon for toggling the sidebar

--- app/redidropper/static/js/sidebar.js
+++ app/redidropper/static/js/sidebar.js
@@ -3,5 +3,10 @@ $(document).ready(function() {
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
+        var icon = $(this).parent().find(".fa")
+        if (icon.hasClass('fa-angle-left'))
+            icon.removeClass('fa-angle-left').addClass("fa-angle-right");
+        else
+            icon.removeClass('fa-angle-right').addClass("fa-angle-left");
     });
 });
